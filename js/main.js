e9b02e05577af36d8a8c9f48193e7574b7e283b1
kauanelaurindo <108004992+kauanelaurindo@users.noreply.github.com>
2024-10-25 10:48:18 -0300

    Add files via upload
    25/10

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import { aleatorio } from "../aleatorio";
-import { perguntas } from "./perguntas";
+import { aleatorio } from "./aleatorio.js";
+import { perguntas } from "./perguntas.js";
 
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
@@ -7,6 +7,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 
 let atual = 0;
@@ -36,7 +37,7 @@ function mostraAlternativas() {
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes =  aleatorio(opcaoSelecionada.afirmacao);
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
@@ -46,10 +47,16 @@ function mostraResultado() {
     caixaPerguntas.textContent = "No Final...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = " ";
+    caixaResultado.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogarNovamente);
+}
+
+function jogarNovamente(){
+    atual = 0;
+    historiaFinal="";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
 }
 
 
-
 mostraPergunta();
-
-
